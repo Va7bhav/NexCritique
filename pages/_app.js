4ab2fc66e3f690e40bdfe0f22fdf534b913bc4cd
Vaibhav <vbrana2014@gmail.com>
2024-02-02 02:59:53 +0530

    17:32 #43

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,60 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [cart, setCart] = useState({})
+  const [subTotal, setSubTotal] = useState(0)
+  useEffect(() => {
+    try {
+      if (localStorage.getItem("cart")) {
+        setCart(JSON.parse(localStorage.getItem("cart")))
+      } 
+    } catch (error) {
+      console.log(error)
+      localStorage.clear()
+    }
+  }, [])
+  
+  const saveCart = () => {
+    localStorage.setItem("cart", cart);
+
+    let subt = 0;
+    let keys = Object.keys(cart)
+    for (let i = 0; i < keys.length; i++) {
+      subt += cart[keys[i]].price * cart[keys[i]].qty;
+    }
+    setSubTotal(subt);
+  }
+  const addToCart = (itemCode, qty, price, name, size, variant) => {
+    let newCart = cart;
+    if (itemCode in cart) {
+      newCart[itemCode].qty = cart[itemCode].qty + qty;
+    } else {
+      newCart[itemCode] = {qty: 1, price, name, size, variant}
+    }
+    setCart(newCart)
+    saveCart(newCart)
+  }
+  const clearCart = () => {
+    setCart({})
+    saveCart({})
+  }
+  const removeFromCart = (itemCode, qty, price, name, size, variant) => {
+    let newCart = cart;
+    if (itemCode in cart) {
+      newCart[itemCode].qty = cart[itemCode].qty - qty;
+    } 
+    if (newCart[itemCode]["qty"] <= 0) {
+      delete newCart[itemCode];
+    }
+    setCart(newCart)
+    saveCart(newCart)
+  }
   return <>
-    <Navbar />
-    <Component {...pageProps} />;
+    <Navbar cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart}  subTotal={subTotal}/>
+    <Component cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} {...pageProps} subTotal={subTotal}/>;
     <Footer />
   </>
 }
