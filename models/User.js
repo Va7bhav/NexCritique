08894fe1af4a7252a02742de9a106bcd57794ccb
Vaibhav <vbrana2014@gmail.com>
2024-02-04 00:53:21 +0530

    completed models folder #47 complete

---
+++ models/User.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-undef
+const mongoose = require("mongoose");
+
+// #10:56
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+}, { timestamps: true });
+
+export default mongoose.model("User", UserSchema);
